27f279abd336a29bc02a09075b4a617c6c2baf53
MBarbara-lab <melissa.cursos.me@gmail.com>
2025-05-11 22:01:47 -0300

    ...

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { CarTableComponent } from '../../components/car-table/car-table.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CardComponent],
+  imports: [NavbarComponent, SidebarComponent, CardComponent, CarTableComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SidebarComponent],
+  imports: [NavbarComponent, SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
